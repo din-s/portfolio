401d4c7c7918212b70dbc623ceb451b5f05b0083
din-s <46292283+din-s@users.noreply.github.com>
2023-09-29 20:33:31 +0530

    Added module structure for stackblitz

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { SharedModule } from './components/shared/shared.module';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   templateUrl: "./main.component.html"
 })
 export class App {
